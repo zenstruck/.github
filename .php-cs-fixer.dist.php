271a99ff0ae91744f58f662904d253b048c9dd7c
Kevin Bond <kevinbond@gmail.com>
2024-05-16 09:51:50 -0400

    enable parallel processing with php-cs-fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -10,6 +10,7 @@ $config = new PhpCsFixer\Config();
 $package = \json_decode(\file_get_contents(__DIR__.'/composer.json'), true)['name'];
 
 return $config
+    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
     ->setRules([
         '@Symfony' => true,
         '@Symfony:risky' => true,
